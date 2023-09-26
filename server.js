63dd45fa6ae3e2cf226d5ddb62281bc0d36bc8f0
jessicakane <jessicakane213@gmail.com>
2023-09-26 15:14:48 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+require('dotenv').config();
+const path = require('path');
+
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT;
+
