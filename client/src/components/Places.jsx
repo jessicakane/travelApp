3c238ace09b602512b267cf72f77a610c029cbf0
jessicakane <jessicakane213@gmail.com>
2023-09-27 11:08:56 +0300

    map component

---
+++ client/src/components/Places.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
+
+export const Places = ({setTravelLoc}) => {
+
+  const {ready, value, setValue, suggestions: {status, data}, clearSuggestions} = usePlacesAutocomplete();
+
+  const handleSelect = async(location) => {
+    setValue(location, false);
+    clearSuggestions();
+    const results = await getGeocode({address: location});
+    const {lat, lng} = await getLatLng(results[0]);
+    setTravelLoc({lat, lng});
+  }
+
+  return (
+    <Combobox onSelect = {handleSelect}>
+      <ComboboxInput value = {value} onChange = {e => setValue(e.target.value)} className = 'combobox-input' placeholder='Travel Destination'/>
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === 'OK' && data.map(({place_id, description}) => <ComboboxOption place_id = {place_id} value = {description}/>)}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  )
+}
