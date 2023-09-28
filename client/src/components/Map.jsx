00b48f539fe82cd74553db78bfc854d6528d4729
nunjachomp <nunjachomp@gmail.com>
2023-09-28 12:16:56 +0300

    Zoom

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -303,7 +303,7 @@ export const Map = () => {
       <div className = 'map'>
       {cityStats && <div className = 'displayStats'>{cityName} <br/><strong>Theft score:</strong> {' ' + cityStats.theft_score}<br/> <strong>Assault score: </strong>{cityStats.assault_score} <br/> <strong>Medical facilities</strong>: {medicalFacilities.length}</div>}
       
-        <GoogleMap options = {options} mapContainerClassName = 'map-container' zoom = {10} center = {userLocation || { lat: 0, lng: 0 }} onLoad={onLoad}> 
+        <GoogleMap options = {options} mapContainerClassName = 'map-container' zoom = {8} center = {userLocation || { lat: 0, lng: 0 }} onLoad={onLoad}> 
 
         {directions && <DirectionsRenderer directions={directions} options={{
           polylineOptions: {
