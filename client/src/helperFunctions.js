3b0ae737ac2e7beea6156e603cab7f31764713f3
jessicakane <jessicakane213@gmail.com>
2023-09-28 09:42:57 +0300

    more colors

--- client/src/helperFunctions.js
+++ client/src/helperFunctions.js
@@ -28,15 +28,48 @@ export const getCityNameFromCoordinates = (latitude, longitude) => {
     });
   }
 
+  export const getLocationDetailsFromCoordinates = (latitude, longitude) => {
+    return new Promise((resolve, reject) => {
+      const apiKey = 'AIzaSyD4wav1ju33YJcb_sz0K1m1q-jsBUC2hYA';
+      const geocodingEndpoint = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`;
+  
+      fetch(geocodingEndpoint)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.status === 'OK' && data.results.length > 0) {
+            const addressComponents = data.results[0].address_components;
+            const cityComponent = addressComponents.find((component) =>
+              component.types.includes('locality')
+            );
+            const districtComponent = addressComponents.find((component) =>
+              component.types.includes('administrative_area_level_2')
+            );
+            if (cityComponent && districtComponent) {
+              const city = cityComponent.long_name;
+              const district = districtComponent.long_name;
+              resolve({ city, district });
+            } else {
+              reject('City or district not found in geocoding results.');
+            }
+          } else {
+            reject('Location not found.');
+          }
+        })
+        .catch((error) => {
+          reject('Error fetching data from Google Maps API.');
+        });
+    });
+  };
+
 export const generateCircularPoints = (center, radius, numPoints) => {
     const points = [];
     const angleIncrement = (2 * Math.PI) / numPoints; 
   
     for (let i = 0; i < numPoints; i++) {
       const angle = i * angleIncrement;
-      const x = center.lat + radius * Math.cos(angle);
+      const x = center[0] + radius * Math.cos(angle);
      
-      const y = center.lng + radius * Math.sin(angle);
+      const y = center[1] + radius * Math.sin(angle);
      
       points.push([x,y]);
     }
